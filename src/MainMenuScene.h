da09aedfe1402a548092f1dba6f186937edfabf6
Zelcon Games <github@zelcongames.com>
2012-02-02 19:01:06 -0800

    Added the file skeleton for the menu setup

---
+++ src/MainMenuScene.h
@@ -0,0 +1,14 @@
+#ifndef SCENE_LOADER_TEST_H
+#define SCENE_LOADER_TEST_H
+#pragma once
+
+#include <Scene/State.hpp>
+
+class Main : public dt::State {
+    Q_OBJECT
+public:
+    void OnInitialize();
+    void UpdateStateFrame(double simulation_frame_time);
+};
+
+#endif
