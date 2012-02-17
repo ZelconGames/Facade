1b1d3a2be9c2e82b2dd18781e0b344fcd545576e
shua <shualloret@gmail.com>
2012-02-16 21:19:36 -0500

    folder and scene file specification cleanup

--- src/SceneLoaderTest.cpp
+++ src/SceneLoaderTest.cpp
@@ -6,16 +6,32 @@
 
 void Main::OnInitialize() 
 {
-    dt::ResourceManager::Get()->AddDataPath(QDir("C:\\users\\EnterUserNameHere\\Facade\\bin\\debug\\data\\"));
+    {
+        QDir dir(QCoreApplication::applicationDirPath());
+        while(!dir.isRoot()) {
+            QDir data(dir.absolutePath() + "/externals/ducttape-engine");
+            if(data.exists()) {
+                ResourceManager::Get()->AddDataPath(data);
+
+                break;
+            }
+            dir.cdUp();
+        }
+    }
     dt::ResourceManager::Get()->AddResourceLocation("","FileSystem");
     Ogre::ResourceGroupManager::getSingleton().initialiseAllResourceGroups();
 
     dt::Scene* scene = nullptr;
-    scene = SceneLoader::LoadScene("C:\\users\\EnterUserNameHere\\Facade\\bin\\debug\\data\\test.scene");
+    scene = SceneLoader::LoadScene(Utils::ToString(scenefile));
     if(scene)
         AddScene(scene);
 }
 
 void Main::UpdateStateFrame(double simulation_frame_time) 
 {
+}
+
+void Main::SetFile(std::string file) {
+    scenefile = file;
+    Logger::Get().Debug("Setting scene file: " + Utils::ToString(scenefile));
 }
